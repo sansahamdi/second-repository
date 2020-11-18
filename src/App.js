49d78988f0f4d497e10a84abe993d56be71fbeb2
SANSA HAMDI <sansa.hamdi@gmail.com>
2020-11-18 10:49:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-form">
+      <div className="container">
+        <h1 className="text-center"><span className="font-weight-bold">amazone</span>.com</h1>
+      <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+  </div>
+  <button className="mb-3 btn-primary btn-block" type="submit" class="btn btn-primary">Log in</button>
+  <div className="text-center">
+    <a href="/sign up">Sign Up</a>
+    <span className="p-2">|</span>
+    <a href="/sign up">Forgot Password</a>
+  </div>
+</form>
+      </div>
     </div>
   );
 }
